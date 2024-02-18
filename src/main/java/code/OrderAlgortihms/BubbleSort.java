42bcd421603f48389e62c0663cbac02ffe840136
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-02-17 22:33:23 -0300

    feat: bubble sort

--- src/main/java/code/OrderAlgortihms/BubbleSort.java
+++ src/main/java/code/OrderAlgortihms/BubbleSort.java
@@ -1,7 +1,17 @@
-package org.OrderAlgortihms;
+package code.OrderAlgortihms;
 
 public class BubbleSort {
     private BubbleSort() {}
 
-    public static void sort(int[] v){}
+    public static void sort(int[] v){
+        for(int i = 0; i < v.length; i++){
+            for(int j = 1; j < v.length; j++){
+                if(v[j - 1] > v[j]){
+                    int aux = v[j];
+                    v[j] = v[j - 1];
+                    v[j - 1] = aux;
+                }
+            }
+        }
+    }
 }
