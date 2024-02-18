ea14b1c3dbe52b80e51261aaf2e4eabf3366c4a1
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-02-17 22:41:34 -0300

    feat: insertion sort

--- src/main/java/code/OrderAlgortihms/InsertionSort.java
+++ src/main/java/code/OrderAlgortihms/InsertionSort.java
@@ -1,4 +1,17 @@
-package org.OrderAlgortihms;
+package code.OrderAlgortihms;
 
 public class InsertionSort {
+    private InsertionSort() {}
+    public static void sort(int[] v){
+        for(int i = 1; i < v.length; i++){
+
+            int p = v[i];
+            int j;
+
+            for(j = i -1; j>=0 && v[j] > p; j--)
+                v[j + 1] = v[j];
+
+            v[j + 1] = p;
+        }
+    }
 }
